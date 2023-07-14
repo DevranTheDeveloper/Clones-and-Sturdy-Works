269fb5c459c3c344d8d0934442fb81ee0c1f5e3b
Devran Sever <137220822+DevranTheDeveloper@users.noreply.github.com>
2023-07-14 12:31:02 +0300

    music store

---
+++ 6. Hafta/5. Gün/app.js
@@ -0,0 +1,60 @@
+//* js kodlarımı daha kısa hale getirerek yamamızı saülar $ bunla
+
+// let tagTitle = document.querySelector("button");
+// console.log("tag", tagTitle)
+
+// //TODO JQuery yazımı
+
+// let tag = $("button")
+
+// //* id kullanma 
+
+// console.log($("#a"))
+
+// //* sınıf kullanarak element seçimi
+
+// console.log($(".b"))
+
+// //* birden fazla seçici kullanarak element seçimi
+
+// let color = $("div")
+// color.css("background-color", "red")
+
+// //elementlere ulaşma
+// $("ul li")
+
+//* element değiştirme
+// $("#b").text("Naber")
+
+// color.css("color", "blue")
+
+//* width ve heigth değerlerini değiştirme
+// $("div").width(300)
+// $("div").height(300)
+
+//* birden fazla css 
+
+// $("div").css({
+//     "background-color": "blue",
+//     "font-size": "3rem",
+//     "color": "white",
+// })
+
+// $("div").addClass("eles")
+
+// $("div").append("<p>Yeni bir paragraf</p>")
+
+// $("div").remove()
+
+// //* örnek
+
+// let ornek = $("#ornek")
+
+// ornek.css({
+//     "background-color": "red",
+//     "color": "white",
+//     "font-size": "300",
+//     "width": "fit-content",
+// })
+// ornek.append("<h1>Sa</h1>")
+
---
+++ Music_Store/script.js
@@ -0,0 +1,18 @@
+const logine = document.querySelector("#login-email");
+const loginp = document.querySelector("#login-password");
+const logins = document.querySelector("#login-submit");
+
+logins.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    const get = localStorage.getItem("user");
+    const sorgu = JSON.parse(get);
+
+    if (sorgu && sorgu[0].email === logine.value && sorgu[0].password === loginp.value) {
+        console.log("Giriş yapıldı");
+    } else {
+        document.querySelector("#err").textContent = "Password or Email is incorrect"
+        document.querySelector("#err").style.color = "red"
+        return;
+    }
+});
