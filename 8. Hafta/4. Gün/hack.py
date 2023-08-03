2933c14ff5c01b08965b74608bbd426a1a4b1ef9
Devran Sever <137220822+DevranTheDeveloper@users.noreply.github.com>
2023-08-03 20:03:27 +0300

    asd

--- 8. Hafta/4. Gün/hack.py
+++ 8. Hafta/4. Gün/hack.py
@@ -3,31 +3,35 @@ import time
 
 time.sleep(5)
 
+i = 0
 
 def mesaf():
-    pyautogui.moveTo(x=1820,y=13)
-    time.sleep(0.1)
-    pyautogui.click()
-    time.sleep(0.1)
-    pyautogui.moveTo(x=1269, y=1050)
-    time.sleep(0.1)
-    pyautogui.click()
-    time.sleep(0.1)
-    pyautogui.moveTo(x=542, y=747)
-    time.sleep(0.1)
-    pyautogui.click()
-    time.sleep(0.1)
-    pyautogui.write("deneme")
-    time.sleep(0.1)
-    pyautogui.moveTo(x=566, y=897)
-    time.sleep(0.1)
-    pyautogui.click()
-    time.sleep(0.1)
-    pyautogui.moveTo(x=1011, y=310)
-    time.sleep(0.1)
-    pyautogui.click()
+    # pyautogui.moveTo(x=1820,y=13)
+    # time.sleep(0.1)
+    # pyautogui.click()
+    # time.sleep(0.1)
+    # pyautogui.moveTo(x=1269, y=1050)
+    # time.sleep(0.1)
+    # pyautogui.click()
+    # time.sleep(0.1)
+    # pyautogui.moveTo(x=542, y=747)
+    # time.sleep(0.1)
+    # pyautogui.click()
+    # time.sleep(0.1)
+    # pyautogui.write("deneme")
+    # time.sleep(0.1)
+    # pyautogui.moveTo(x=566, y=897)
+    # time.sleep(0.1)
+    # pyautogui.click()
+    # time.sleep(0.1)
+    # pyautogui.moveTo(x=1011, y=310)
+    # time.sleep(0.1)
+    # pyautogui.click()
+    pyautogui.write("sa")
+    pyautogui.press("enter")
     
-    
-while True:
-    mesaf()    
-    time.sleep(100)
+
+while i < 10: 
+    mesaf()
+    time.sleep(0)
+    i += 1
