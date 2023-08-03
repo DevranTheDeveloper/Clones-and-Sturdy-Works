ff8a44307074d3153263b6a86a39f7bde8773878
Devran Sever <137220822+DevranTheDeveloper@users.noreply.github.com>
2023-08-03 17:54:50 +0300

    deneme

--- 8. Hafta/4. Gün/hack.py
+++ 8. Hafta/4. Gün/hack.py
@@ -3,19 +3,29 @@ import time
 
 time.sleep(5)
 
+
 def mesaf():
-    pyautogui.write("N")
-    time.sleep(2)
-    pyautogui.write("A")
-    time.sleep(2)
-    pyautogui.write("B")
-    time.sleep(2)
-    pyautogui.write("E")
-    time.sleep(2)
-    pyautogui.write("R")
-    pyautogui.press("enter")
+    pyautogui.moveTo(x=1820,y=13)
+    time.sleep(1)
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.moveTo(x=1269, y=1050)
+    time.sleep(1)
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.moveTo(x=542, y=747)
+    time.sleep(1)
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.write("deneme")
+    time.sleep(1)
+    pyautogui.moveTo(x=566, y=897)
+    time.sleep(1)
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.moveTo(x=1011, y=310)
     
     
 while True:
     mesaf()    
-    time.sleep(0)
+    time.sleep(100)
