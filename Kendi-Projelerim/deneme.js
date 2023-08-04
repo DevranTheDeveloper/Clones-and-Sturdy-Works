7239d2809eeda2947b6bc51119a63bf6d94900cd
Devran Sever <137220822+DevranTheDeveloper@users.noreply.github.com>
2023-08-04 10:10:28 +0300

    denemedeneme

--- Kendi-Projelerim/deneme.js
+++ Kendi-Projelerim/deneme.js
@@ -1,4 +1,13 @@
-document.getElementById("genisleyenButton").addEventListener("click", function() {
-    document.getElementById("kutu").classList.toggle("genislemis");
-  });
-  
+//* zorluk derecesi
+
+const yks2021 = 7  
+const yks2022 = 4
+const yks2023 = 6
+
+if (yks2021 > yks2022) {
+  alert("2021, 2022 den daha zordu bu yüzden puanlar 2022 de uçtu")
+} else if (yks2022 > yks2023) {
+  alert("2022, 2023 den daha zordu bu yüzden puanlar yine uçtu")
+} else {
+  alert("2023, 2022 den daha zordu bu yüzden puan değişikliği yine var fakat daha az")
+}
