eba248dce56db6abfc24081e545e1dc3fb2cb5ce
Devran Sever <137220822+DevranTheDeveloper@users.noreply.github.com>
2023-08-11 11:27:15 +0300

    asd

--- 9. Hafta/4. Gün/main.py
+++ 9. Hafta/4. Gün/main.py
@@ -1,22 +1,34 @@
-for sayi in range(1, 11, 2):
-    print(sayi)
-
-sayi = int(input("Sayı Gir: "))
-toplam = 0 
-while sayi > 0:
-    toplam += sayi 
-    sayi -= 1
-print(toplam)
+# for sayi in range(1, 11, 2):
 
 
-tr = []
-toplam = 0
-while True:
-    sayi = int(input("Sayı Gir: "))
-    tr.insert(0, sayi)
-    toplam += sayi
-    if sayi == 0:
-        break
-print(toplam)
-print(tr)
-    
+
+
+#     print(sayi)
+
+# sayi = int(input("Sayı Gir: "))
+# toplam = 0 
+# while sayi > 0:
+#     toplam += sayi 
+#     sayi -= 1
+# print(toplam)
+
+
+# tr = []
+# toplam = 0
+# while True:
+#     sayi = int(input("Sayı Gir: "))
+#     tr.insert(0, sayi)
+#     toplam += sayi
+#     if sayi == 0:
+#         break
+# print(toplam)
+# print(tr)
+
+# text = "Bugün python tekrar yapıyoz"
+# print (f"Bu textte {text.count('a')} tane a, {text.count('e')} tane e vardır.")
+
+text = "Her defasında ekileri biçenler ekmek için yemek yemeye geldiler."
+list = text.split()
+for eleman in list:
+    e = eleman.count("e")
+    print(f"{eleman} kelimede {e} tane e var.")
